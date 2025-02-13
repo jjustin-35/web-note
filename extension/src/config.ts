11f9305e5bdf49113fc4185c55145deee979bb5b
jjustin-chen <justin.chen@kdanmobile.com>
2025-02-13 23:00:26 +0800

    update: seperate extension

--- extension/src/config.ts
+++
@@ -1,2 +0,0 @@
-export const DASHBOARD_URL = 'http://localhost:5173';
-export const API_URL = 'http://localhost:5173/api';
--- extension/src/sidebar.ts
+++
@@ -1,5 +0,0 @@
-import Sidebar from './components/Sidebar.svelte';
-
-new Sidebar({
-  target: document.getElementById('app')!,
-});
