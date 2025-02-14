eab7019f3aea71337b18d05af0949115df1618f8
jjustin-chen <justin.chen@kdanmobile.com>
2025-02-14 09:06:41 +0800

    update: config

--- src/config.ts
+++ src/config.ts
@@ -1,1 +1,12 @@
-export const API_BASE_URL = '/api';
+let API_BASE_URL = "/api";
+let HOST = "http://localhost:5173";
+
+switch (process.env.NODE_ENV) {
+  case "production":
+    HOST = "https://web-note-nine.vercel.app";
+    break;
+  default:
+    break;
+}
+
+export { API_BASE_URL, HOST };
